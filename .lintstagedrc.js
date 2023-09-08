66d41c20fdcb7c620225569b5dad4d5a71394b18
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-08 12:22:28 +0200

    Add fist test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,5 +3,6 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `npm run lint --fix . ${filenames.join(' --file')}`,
     `prettier --write ${filenames.join(' ')}`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`,
   ],
 };
---
+++ src/components/test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Main from './Main';
+
+describe('<Main/>', () => {
+  it('should render the heading', () => {
+    render(<Main />);
+    expect(
+      screen.getByRole('heading', { name: /won games/i })
+    ).toBeInTheDocument();
+  });
+});
