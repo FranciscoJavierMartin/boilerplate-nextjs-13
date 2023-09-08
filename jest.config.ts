c99fec86060e152cd0e03a424592ddfa36f2463e
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-08 14:32:18 +0200

    Test config for styles

--- jest.config.ts
+++ jest.config.ts
@@ -22,7 +22,11 @@ const config: Config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/app/**',
+    '!src/lib/registry.tsx',
+  ],
 
   // The directory where Jest should output its coverage files
   // coverageDirectory: undefined,
